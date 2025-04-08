011ea3e65051458729e1aadfec3b1d232099f279
Branislav Bogosavac <97567320+branislav999@users.noreply.github.com>
2025-04-08 04:44:48 -0600

    'bcrypt'

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,7 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+
 const bcrypt = require('bcrypt');
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const { pool } = require('./db');
 
 const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
